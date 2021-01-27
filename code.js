41e673879dc86f21ba4ed41559a4dd988f4113dd
Elijah Bunkley <eli98711@gmail.com>
2021-01-26 21:18:12 -0500

    Added scripts to handle moves and player turns

--- code.js
+++ code.js
@@ -1,10 +1,30 @@
+const PLAYER_ONE_SYMBOL = 'X';
+const PLAYER_TWO_SYMBOL = 'O';
+
 class TicTacToeGame {
 	
+	
+	handleSquareClick(event){
+		this.executeMove(event.target.id);
+	}
+
+	executeMove(moveIndex){
+		if(this.board[moveIndex] == ""){
+			this.board[moveIndex] = this.currentPlayer;
+			this.currentPlayer = (this.currentPlayer == PLAYER_ONE_SYMBOL ?
+				                   PLAYER_TWO_SYMBOL :
+				                   PLAYER_ONE_SYMBOL);
+			console.log(this.board);
+		}
+	}
+
+
 	drawBoard(){
 		document.body.innerHTML = "";
 		let gameBoard = document.createElement('div');
 		gameBoard.id ='gameBoard';
 		gameBoard.classList.add('board');
+		gameBoard.addEventListener('click', this.handleSquareClick.bind(this));
 
 		this.board.forEach((square, index) => {
 			let squareElement = document.createElement('div')
@@ -19,7 +39,9 @@ class TicTacToeGame {
 		this.board = ["","","",
 		              "","","",
 		              "","",""];
+		this.currentPlayer = PLAYER_ONE_SYMBOL;
 		this.drawBoard();
+
 	}
 }
 
